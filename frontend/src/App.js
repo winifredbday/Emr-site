2b5b6ab571d8948fc2ccb0cb37cdee3fded4dc56
sdabbey <dehydrus223@gmail.com>
2024-07-29 03:35:02 +0000

    Set up frontend code structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { CssVarsProvider } from '@mui/joy/styles';
+import Sheet from '@mui/joy/Sheet';
+import Typography from '@mui/joy/Typography';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Input from '@mui/joy/Input';
+import Button from '@mui/joy/Button';
+import Link from '@mui/joy/Link';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CssVarsProvider>
+      <Sheet
+        sx={{
+          width: 300,
+          mx: 'auto', // margin left & right
+          my: 4, // margin top & bottom
+          py: 3, // padding top & bottom
+          px: 2, // padding left & right
+          display: 'flex',
+          textAlign: 'center',
+          flexDirection: 'column',
+          gap: 2,
+          borderRadius: 'sm',
+          boxShadow: 'md',
+        }}
+      >
+        <div>
+          <Typography level="h4" component="h1">
+            Welcome!
+          </Typography>
+          <Typography level="body-sm">Sign in to continue.</Typography>
+        </div>
+
+        <FormControl>
+          <FormLabel>Email</FormLabel>
+          <Input name="email" type="email" placeholder="johndoe@email.com" />
+        </FormControl>
+        
+        <FormControl>
+          <FormLabel>Password</FormLabel>
+          <Input name="password" type="password" placeholder="password" />
+        </FormControl>
+
+        <Button sx={{ mt: 1 }}>
+          Log in
+        </Button>
+        <Typography
+          endDecorator={<Link href="/sign-up">Sign up</Link>}
+          fontSize="sm"
+          sx={{ alignSelf: 'center'}}
+          >
+            Don't have an account?
+          </Typography>
+      </Sheet>
+    </CssVarsProvider>
   );
 }
 
