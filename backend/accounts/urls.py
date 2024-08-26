fde1b30dfa733e49f15005c2e46c314d20e44cdb
sdabbey <dehydrus223@gmail.com>
2024-08-26 22:52:58 +0000

    update

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import create_patient, SignInView, SignOutView
+from .views import *
 
 urlpatterns = [
     path('signup/', create_patient, name='create_patient'),
     path('signin/', SignInView.as_view(), name='signin'), 
-    path('signout/', SignOutView.as_view(), name='signout')
+    path('signout/', SignOutView.as_view(), name='signout'),
+    path('me/', UserDetailView.as_view(), name='user-detail'),
 ]
